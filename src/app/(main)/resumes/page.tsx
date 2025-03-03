8d92215a21091a4e9b695e1344e4734064072477
FrankBonanno <fb0023@mix.wvu.edu>
2025-03-02 22:09:48 -0500

    project setup

---
+++ src/app/(main)/resumes/page.tsx
@@ -0,0 +1,9 @@
+const ResumesPage = () => {
+  return (
+    <main>
+      <h1>Resumes</h1>
+    </main>
+  );
+};
+
+export default ResumesPage;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Front Page</h1>
+    </main>
+  );
+}
