2d545cf15b899a2dc3e51e80edceb12891fd0a1e
FrankBonanno <fb0023@mix.wvu.edu>
2025-03-03 02:46:17 -0500

    education preview

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -35,6 +35,7 @@ const ResumePreview = ({ resumeData, className }: ResumePreviewProps) => {
         <PersonalInfoHeader resumeData={resumeData} />
         <SummarySection resumeData={resumeData} />
         <WorkExperienceSection resumeData={resumeData} />
+        <EducationSection resumeData={resumeData} />
       </div>
     </div>
   );
@@ -141,6 +142,41 @@ const WorkExperienceSection = ({ resumeData }: ResumeSectionProps) => {
             <p className="text-xs font-semibold">{exp.company}</p>
 
             <div className="whitespace-pre-line text-xs">{exp.description}</div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+const EducationSection = ({ resumeData }: ResumeSectionProps) => {
+  const { educations } = resumeData;
+
+  const educationsNotEmpty = educations?.filter(
+    (edu) => Object.values(edu).filter(Boolean).length > 0,
+  );
+
+  if (!educationsNotEmpty?.length) return null;
+
+  return (
+    <>
+      <hr className="border-2" />
+      <div className="space-y-3">
+        <p className="text-lg font-semibold">Education</p>
+
+        {educationsNotEmpty.map((edu, index) => (
+          <div key={index} className="break-inside-avoid space-y-1">
+            <div className="flex items-center justify-between text-sm font-semibold">
+              <span>{edu.degree}</span>
+              {edu.startDate && (
+                <span>
+                  {edu.startDate &&
+                    `${formatDate(edu.startDate, "MM/yyyy")} ${edu.endDate ? `- ${formatDate(edu.endDate, "MM/yyyy")}` : ""}`}
+                </span>
+              )}
+            </div>
+
+            <p className="text-xs font-semibold">{edu.school}</p>
           </div>
         ))}
       </div>
