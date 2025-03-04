3ea12d135682087a72acd573899fdec9e2437558
FrankBonanno <fb0023@mix.wvu.edu>
2025-03-04 04:38:37 -0500

    await the await

--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -49,7 +49,9 @@ async function handleSesionCompleted(session: Stripe.Checkout.Session) {
     throw new Error("User ID is missing in session metadata.");
   }
 
-  (await clerkClient()).users.updateUserMetadata(userId, {
+  await (
+    await clerkClient()
+  ).users.updateUserMetadata(userId, {
     privateMetadata: {
       stripeCustomerId: session.customer as string,
     },
